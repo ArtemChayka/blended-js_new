4fedee0312be6d1faf0d0e655ffad8e383a1ec97
Artem Chaychenko <drchaychenko@gmail.com>
2025-06-30 21:01:38 +0300

    4

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,10 +1,19 @@
 const body = document.querySelector('body')
 const changeBtn = document.querySelector('.theme-toggle-button')
-
+const LS_KEY_2 = 'theme'
 
 changeBtn.addEventListener('click', () => {
     body.classList.toggle('theme-light')
     body.classList.toggle('theme-dark')
-    
+    localStorage.setItem(LS_KEY_2, body.getAttribute('class'))
 })
+
+const LS = localStorage.getItem(LS_KEY_2)
+const getA = body.getAttribute('class')
+
+
+if (LS) {
+    body.classList.replace(getA, LS)
+}
+
 // localStorage.clear()
